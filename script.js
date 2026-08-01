a52dc1f4e51afd83e3dbf47deb4038150d2b82e3
Shivani Dwivedi <princi6124@gmail.com>
2026-08-01 23:28:21 +0530

    feat: files for webpage

---
+++ script.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+            behavior: "smooth"
+
+        });
+
+    });
+
+});
